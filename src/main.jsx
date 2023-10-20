29777c736e0ad0a5671cb8308efe2fd481863bab
markopaunovic1 <marpau970514@hotmail.com>
2023-10-20 14:16:45 +0200

    Changed to HashRouter due to some minor paths routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './features/rootreducer.js';
 import { Provider } from 'react-redux';
